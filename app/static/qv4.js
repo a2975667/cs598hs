056cc271c15481f9f77b3f9559a8652d434bea3e
tcheng <tcheng@link.cuhk.edu.hk>
2019-04-04 16:39:33 -0500

    intergrate likert

--- app/static/qv4.js
+++ app/static/qv4.js
@@ -1,12 +1,12 @@
 function qv_cal(number){	
-	if (number<4){
-		return 1;
+	if (number<3){
+		return number;
 	}else{
-		return Math.pow(number-2, 2);
+		return Math.pow(number-2, 2)+2;
 	}
 }
 
 function qv_total_voice(){
 	var total_votes = $(".qv_cost").length
-	return total_votes
+	return total_votes*2
 }
