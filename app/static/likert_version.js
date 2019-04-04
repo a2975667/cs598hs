6f53af64d2a34f692ab27412085391e5950b7cfa
tiffanylwt <wl439@cornell.edu>
2019-04-04 15:53:29 -0500

    added likert version of questions

---
+++ app/static/likert_version.js
@@ -0,0 +1,207 @@
+var json_info = {
+    pages: [{
+            title: "Suppose that you use only your home computer to access the WebMD website, and that nobody else uses this computer. Based only on the information that your read above, please answer the questions below indicating what information you would allow XYZ Advertising Company to collect for the purpose of showing you targeted ads only on the WebMD website.",
+            questions: [{
+                    type: "matrix",
+                    name: "Q20_computer",
+                    title: "I would be willing to allow XYZ Advertising Company to use and store the following information about my computer. This information will be retained for one day.",
+                    columns: [{
+                            value: -2,
+                            text: "Strongly Disagree"
+                        },
+                        {
+                            value: -1,
+                            text: "Disagree"
+                        },
+                        {
+                            value: 0,
+                            text: "Neutral"
+                        },
+                        {
+                            value: 1,
+                            text: "Agree"
+                        },
+                        {
+                            value: 2,
+                            text: "Strongly Agree"
+                        }
+                    ],
+                    rows: [{
+                            value: "IP",
+                            text: "The IP address of my computer (i.e., a computer identifier assigned by your Internet service provider)"
+                        },
+                        {
+                            value: "browser",
+                            text: "The name and version of the web browser (e.g., Internet Explorer 9, Firefox 18.0.1, Safari 6.0.2, etc.) that I use to visit the WebMD website"
+                        }
+                    ]
+                },
+                {
+                    type: "matrix",
+                    name: "Q21_demographic",
+                    title: "I would be willing to allow XYZ Advertising Company to use and store the following demographic and preference information. This information will be retained for one day.",
+                    columns: [{
+                            value: -2,
+                            text: "Strongly Disagree"
+                        },
+                        {
+                            value: -1,
+                            text: "Disagree"
+                        },
+                        {
+                            value: 0,
+                            text: "Neutral"
+                        },
+                        {
+                            value: 1,
+                            text: "Agree"
+                        },
+                        {
+                            value: 2,
+                            text: "Strongly Agree"
+                        }
+                    ],
+                    rows: [{
+                            value: "gender",
+                            text: "My gender"
+                        },
+                        {
+                            value: "income",
+                            text: "My income bracket"
+                        },
+                        {
+                            value: "religion",
+                            text: "My religion"
+                        },
+                        {
+                            value: "marital",
+                            text: "My marital status"
+                        },
+                        {
+                            value: "hobby",
+                            text: "My hobbies"
+                        }
+                    ]
+                },
+                {
+                    type: "matrix",
+                    name: "Q22_web activity",
+                    title: "I would be willing to allow XYZ Advertising Company to use and store the following information related to my interactions with the WebMD website. This information will be retained for one day.",
+                    columns: [{
+                            value: -2,
+                            text: "Strongly Disagree"
+                        },
+                        {
+                            value: -1,
+                            text: "Disagree"
+                        },
+                        {
+                            value: 0,
+                            text: "Neutral"
+                        },
+                        {
+                            value: 1,
+                            text: "Agree"
+                        },
+                        {
+                            value: 2,
+                            text: "Strongly Agree"
+                        }
+                    ],
+                    rows: [{
+                            value: "page",
+                            text: "The pages I've visited on the WebMD website"
+                         },
+                         {
+                            value: "weight height",
+                            text: "My weight and height"
+                         },
+                         {
+                            value: "time",
+                            text: "How long I spent on each page of the WebMD website"
+                         }
+                    ]
+                },
+                {
+                    type: "matrix",
+                    name: "Q23_location",
+                    title: "I would be willing to allow XYZ Advertising Company to use and store the following information related to my location. This information will be retained for one day.",
+                    columns: [{
+                            value: -2,
+                            text: "Strongly Disagree"
+                        },
+                        {
+                            value: -1,
+                            text: "Disagree"
+                        },
+                        {
+                            value: 0,
+                            text: "Neutral"
+                        },
+                        {
+                            value: 1,
+                            text: "Agree"
+                        },
+                        {
+                            value: 2,
+                            text: "Strongly Agree"
+                        }
+                    ],
+                    rows: [{
+                            value: "country",
+                            text: "The country from which I'm visiting the WebMD website"
+                         },
+                         {
+                            value: "city",
+                            text: "The town or city from which I'm visiting the WebMD website"
+                         },
+                         {
+                            value: "address",
+                            text: "The exact address from which I'm visiting the WebMD website"
+                         }
+                    ]
+                },
+                {
+                    type: "matrix",
+                    name: "Q24_pii",
+                    title: "I would be willing to allow XYZ Advertising Company to collect the following information. This information will be retained for one day.",
+                    columns: [{
+                            value: -2,
+                            text: "Strongly Disagree"
+                        },
+                        {
+                            value: -1,
+                            text: "Disagree"
+                        },
+                        {
+                            value: 0,
+                            text: "Neutral"
+                        },
+                        {
+                            value: 1,
+                            text: "Agree"
+                        },
+                        {
+                            value: 2,
+                            text: "Strongly Agree"
+                        }
+                    ],
+                    rows: [{
+                            value: "email",
+                            text: "My email address"
+                         },
+                         {
+                            value: "phone",
+                            text: "My phone number"
+                         },
+                         {
+                            value: "credit card",
+                            text: "My credit card number"
+                         }
+                    ]
+                }
+            ]
+        }
+    ]
+};
+
