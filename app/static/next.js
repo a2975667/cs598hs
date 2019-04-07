96bb6f179573bc241294417098a41dc2c58edcf0
tcheng <tcheng@link.cuhk.edu.hk>
2019-04-07 15:57:27 -0500

    change next sequence

--- app/static/next.js
+++ app/static/next.js
@@ -5,12 +5,13 @@ var sequence = {
     "D":['/normal/demographic', '/normal/likert_version','/qv/example', '/qv/2', '/qv/1', '/qv/3', '/normal/followup', '/complete'],
     "E":['/normal/demographic', '/normal/likert_version','/qv/example', '/qv/3', '/qv/1', '/qv/2', '/normal/followup', '/complete'],
     "F":['/normal/demographic', '/normal/likert_version','/qv/example', '/qv/3', '/qv/2', '/qv/1', '/normal/followup', '/complete'],
-    "G":['/normal/demographic','/qv/example', '/qv/1', '/qv/2', '/qv/3', '/normal/likert_version', '/normal/followup', '/complete'],
-    "H":['/normal/demographic','/qv/example', '/qv/1', '/qv/3', '/qv/2', '/normal/likert_version', '/normal/followup', '/complete'],
-    "I":['/normal/demographic','/qv/example', '/qv/2', '/qv/1', '/qv/3', '/normal/likert_version', '/normal/followup', '/complete'],
-    "J":['/normal/demographic','/qv/example', '/qv/2', '/qv/3', '/qv/1', '/normal/likert_version', '/normal/followup', '/complete'],
-    "K":['/normal/demographic','/qv/example', '/qv/3', '/qv/1', '/qv/2', '/normal/likert_version', '/normal/followup', '/complete'],
-    "L":['/normal/demographic','/qv/example', '/qv/3', '/qv/2', '/qv/1', '/normal/likert_version', '/normal/followup', '/complete'],
-    "M":['/normal/demographic', '/normal/likert_version','/qv/example', '/qv/2', '/qv/4', '/normal/followup', '/complete'],
-    "N":['/normal/demographic','/qv/example', '/qv/2', '/normal/likert_version', '/qv/4', '/normal/followup', '/complete']
+    "G":['/normal/demographic', '/normal/likert_version','/qv/example', '/qv/2', '/qv/4', '/normal/followup', '/complete'],
+    "H":['/normal/demographic','/qv/example', '/qv/2', '/normal/likert_version', '/qv/4', '/normal/followup', '/complete'],
+    "I":['/normal/demographic','/qv/example', '/qv/1', '/qv/2', '/qv/3', '/normal/likert_version', '/normal/followup', '/complete'],
+    "J":['/normal/demographic','/qv/example', '/qv/1', '/qv/3', '/qv/2', '/normal/likert_version', '/normal/followup', '/complete'],
+    "K":['/normal/demographic','/qv/example', '/qv/2', '/qv/1', '/qv/3', '/normal/likert_version', '/normal/followup', '/complete'],
+    "L":['/normal/demographic','/qv/example', '/qv/2', '/qv/3', '/qv/1', '/normal/likert_version', '/normal/followup', '/complete'],
+    "M":['/normal/demographic','/qv/example', '/qv/3', '/qv/1', '/qv/2', '/normal/likert_version', '/normal/followup', '/complete'],
+    "N":['/normal/demographic','/qv/example', '/qv/3', '/qv/2', '/qv/1', '/normal/likert_version', '/normal/followup', '/complete']
+
 }
